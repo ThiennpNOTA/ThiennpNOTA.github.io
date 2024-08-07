567ac4a5ad0fa20cc3d86c8e7bd5a62e0e9363d6
tinosuper <tinosuper@yunr.me>
2024-08-07 09:05:54 +0700

    update new package

---
+++ cocos-js/wait-for-ammo-instantiation.js
@@ -0,0 +1,13 @@
+System.register(['./instantiated-317e1377.js'], function (exports) {
+	'use strict';
+	return {
+		setters: [function (module) {
+			exports('default', module.i9);
+		}],
+		execute: function () {
+
+
+
+		}
+	};
+});
